0863ba6105f681324dd0ba86cae8d10a4b84c1b1
Christopher Greening <chris@christophergreening.com>
2023-03-07 21:17:39 -0500

    FIX: fixed wrong amount of arguments to super class

--- spyrograph/deltoid.py
+++ spyrograph/deltoid.py
@@ -5,4 +5,4 @@ from spyrograph.hypocycloid import Hypocycloid
 
 class Deltoid(Hypocycloid):
     def __init__(self, R: Number, thetas: List[Number]) -> None:
-        super().__init__(R, R/3, R/3, thetas)
+        super().__init__(R, R/3, thetas)
