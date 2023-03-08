65e66365f2d078ad4b41329dce8c404818fd9763
Christopher Greening <chris@christophergreening.com>
2023-03-07 21:25:06 -0500

    DOCS: added class docs for Deltoid

--- spyrograph/deltoid.py
+++ spyrograph/deltoid.py
@@ -4,5 +4,8 @@ from typing import List
 from spyrograph.hypocycloid import Hypocycloid
 
 class Deltoid(Hypocycloid):
+    """Model of a deltoid which is a special case of a hypocycloid where the 
+    rolling circle has 1/3 the radius of the fixed circle
+    """
     def __init__(self, R: Number, thetas: List[Number]) -> None:
         super().__init__(R, R/3, thetas)
