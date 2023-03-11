3b4c9b88d34234c4a94d78fc81db5018f4ced73d
Christopher Greening <chris@christophergreening.com>
2023-03-11 00:16:20 -0500

    DOCS: added class docstring to Epicycloid

--- spyrograph/epicycloid.py
+++ spyrograph/epicycloid.py
@@ -1,5 +1,5 @@
-"""Model of an epicycloid. An epicycloid is a specific case of an epitrochoid 
-where the distance from the rolling circle is equal to its radius. 
+"""Model of an epicycloid. An epicycloid is a specific case of an epitrochoid
+where the distance from the rolling circle is equal to its radius.
 """
 
 from typing import List
@@ -8,5 +8,9 @@ from numbers import Number
 from spyrograph.epitrochoid import Epitrochoid
 
 class Epicycloid(Epitrochoid):
+    """Model of a epicycloid which is a special case of a epitrochoid where the
+    circle is rolling around the outside of the fixed circle and has 1/3 the
+    radius of the fixed circle
+    """
     def __init__(self, R: Number, r: Number, thetas: List[Number]) -> None:
         super().__init__(R, r, r, thetas)
