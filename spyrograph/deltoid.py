d08c68e98e4265cfab830d288b7c44238f95d5f9
Christopher Greening <chris@christophergreening.com>
2023-03-11 00:14:05 -0500

    DOCS: added module docstring

--- spyrograph/deltoid.py
+++ spyrograph/deltoid.py
@@ -1,10 +1,14 @@
+"""Model of a deltoid. A deltoid is a special case of a hypocycloid where the
+rolling circle has 1/3 the radius of the fixed circle and the distance traced
+from the rolling circle is equal to the radius of the rolling circle"""
+
 from numbers import Number
 from typing import List
 
 from spyrograph.hypocycloid import Hypocycloid
 
 class Deltoid(Hypocycloid):
-    """Model of a deltoid which is a special case of a hypocycloid where the 
+    """Model of a deltoid which is a special case of a hypocycloid where the
     rolling circle has 1/3 the radius of the fixed circle
     """
     def __init__(self, R: Number, thetas: List[Number]) -> None:
