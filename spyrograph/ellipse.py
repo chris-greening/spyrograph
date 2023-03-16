5d7b424ee86cc457feeba35f21e382eb270e5743
Christopher Greening <chris@christophergreening.com>
2023-03-15 22:44:19 -0400

    ADD: defined an Ellipse object

---
+++ spyrograph/ellipse.py
@@ -0,0 +1,15 @@
+"""Model of an ellipse. An ellipse is a special case of a hypotrochoid where the
+rolling circle is 1/2 the radius of the fixed circle and the distance traced
+from the rolling circle is not equal to the radius of the rolling circle"""
+
+from numbers import Number
+from typing import List
+
+from spyrograph.hypotrochoid import Hypotrochoid
+
+class Ellipse(Hypotrochoid):
+    """Model of an ellipse which is a special case of a hypotrochoid where the
+    rolling circle has 1/2 the radius of the fixed circle
+    """
+    def __init__(self, R: Number, d: Number, thetas: List[Number]) -> None:
+        super().__init__(R, R/2, d, thetas)
