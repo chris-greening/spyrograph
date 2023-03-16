8a563a1b911994e2da5ebc4622e2e6a9a463c7e2
Christopher Greening <chris@christophergreening.com>
2023-03-15 22:58:30 -0400

    ADD: added theta start, stop, and step to deltoid

--- spyrograph/deltoid.py
+++ spyrograph/deltoid.py
@@ -11,5 +11,8 @@ class Deltoid(Hypocycloid):
     """Model of a deltoid which is a special case of a hypocycloid where the
     rolling circle has 1/3 the radius of the fixed circle
     """
-    def __init__(self, R: Number, thetas: List[Number]) -> None:
-        super().__init__(R, R/3, thetas)
+    def __init__(
+            self, R: Number, thetas: List[Number], theta_start: Number = None,
+            theta_end: Number = None, theta_step: Number = None
+        ) -> None:
+        super().__init__(R, R/3, thetas, theta_start, theta_end, theta_step)
