8d858144ee1d9e111a7fd4198981ae0981b7ab66
Christopher Greening <chris@christophergreening.com>
2023-03-16 21:54:07 -0400

    ADD: added definition for a ranuncloid

---
+++ spyrograph/ranuncloid.py
@@ -0,0 +1,19 @@
+"""Model of a ranuncloid. A ranuncloid is a specific case of an epicycloid
+where the fixed circle radius equals 1/5 of the rolling circle.
+"""
+
+from typing import List
+from numbers import Number
+
+from spyrograph.epicycloid import Epicycloid
+
+class Ranuncloid(Epicycloid):
+    """Model of a ranuncloid which is a special case of an epicycloid where the
+    rolling circle is 1/5 the radius of the fixed circle
+    """
+    def __init__(
+            self, R: Number, thetas: List[Number] = None,
+            theta_start: Number = None, theta_stop: Number = None,
+            theta_step: Number = None
+        ) -> None:
+        super().__init__(R, R/5, thetas, theta_start, theta_stop, theta_step)
