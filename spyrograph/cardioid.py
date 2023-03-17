faf2ac7268cc88611c2b517431c35cb12b0f59f5
Christopher Greening <chris@christophergreening.com>
2023-03-16 21:11:35 -0400

    FIX: fixed passing up the inheritance

--- spyrograph/cardioid.py
+++ spyrograph/cardioid.py
@@ -9,11 +9,11 @@ from spyrograph.epicycloid import Epicycloid
 
 class Cardioid(Epicycloid):
     """Model of a cardioid which is a special case of an epicycloid where the
-    circle is rolling around the outside of the fixed circle and d = r
+    two circles are equal in radius
     """
     def __init__(
-            self, R: Number, r: Number, thetas: List[Number] = None,
+            self, R: Number, thetas: List[Number] = None,
             theta_start: Number = None, theta_stop: Number = None,
             theta_step: Number = None
         ) -> None:
-        super().__init__(R, r, thetas, theta_start, theta_stop, theta_step)
+        super().__init__(R, R, thetas, theta_start, theta_stop, theta_step)
