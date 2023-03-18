cf712d0c3d8c7298d4a54f660dedca50a856481b
Christopher Greening <chris@christophergreening.com>
2023-03-18 12:40:44 -0400

    REMOVE: removed special cases from imports aside from hypocycloid

--- tests/test_hypotrochoid.py
+++ tests/test_hypotrochoid.py
@@ -3,10 +3,6 @@ import pytest
 
 from spyrograph.hypotrochoid.hypotrochoid import Hypotrochoid
 from spyrograph.hypotrochoid.hypocycloid import Hypocycloid
-from spyrograph.hypotrochoid.deltoid import Deltoid
-from spyrograph.hypotrochoid.astroid import Astroid
-from spyrograph.hypotrochoid.ellipse import Ellipse
-from spyrograph.hypotrochoid.tusi_couple import TusiCouple
 
 @pytest.fixture()
 def thetas() -> "np.array":
@@ -23,37 +19,71 @@ def hypotrochoid_obj(thetas: "np.array") -> "hypotrochoid.Hypotrochoid":
         thetas=thetas
     )
 
-def test_theta_range() -> None:
-    """Test that passing theta start, stop, step creates an expected list of theta values"""
-    hypotrochoid_obj = Hypotrochoid(
-        R = 300,
-        r = 200,
-        d = 100,
-        theta_start=1,
-        theta_stop=10,
-        theta_step=1
-    )
-    assert all(hypotrochoid_obj.thetas == np.arange(1,10,1))
-
-def test_theta_range_default_step() -> None:
-    """Test that passing theta start, stop, step creates an expected list of theta values"""
-    hypotrochoid_obj = Hypotrochoid(
-        R = 300,
-        r = 200,
-        d = 100,
-        theta_start=0,
-        theta_stop=1
-    )
-
-def test_multiple_thetas_exception(thetas: "np.array") -> None:
-    """Test that passing multiple definitions of setting theta raises a ValueError"""
-    with pytest.raises(ValueError):
-        hypotrochoid_obj = Hypotrochoid(
+class TestHypotrochoid:
+    def test_theta_range(self) -> None:
+        """Test that passing theta start, stop, step creates an expected list of theta values"""
+        obj = Hypotrochoid(
             R = 300,
             r = 200,
             d = 100,
-            thetas=thetas,
             theta_start=1,
             theta_stop=10,
             theta_step=1
         )
+        assert all(obj.thetas == np.arange(1,10,1))
+
+    def test_theta_range_default_step(self) -> None:
+        """Test that passing theta start, stop, step creates an expected list of theta values"""
+        obj = Hypotrochoid(
+            R = 300,
+            r = 200,
+            d = 100,
+            theta_start=0,
+            theta_stop=1
+        )
+
+    def test_multiple_thetas_exception(self, thetas: "np.array") -> None:
+        """Test that passing multiple definitions of setting theta raises a ValueError"""
+        with pytest.raises(ValueError):
+            obj = Hypotrochoid(
+                R = 300,
+                r = 200,
+                d = 100,
+                thetas=thetas,
+                theta_start=1,
+                theta_stop=10,
+                theta_step=1
+            )
+
+class TestHypocycloid(TestHypotrochoid):
+    def test_theta_range(self) -> None:
+        """Test that passing theta start, stop, step creates an expected list of theta values"""
+        obj = Hypocycloid(
+            R = 300,
+            r = 200,
+            theta_start=1,
+            theta_stop=10,
+            theta_step=1
+        )
+        assert all(obj.thetas == np.arange(1,10,1))
+
+    def test_theta_range_default_step(self) -> None:
+        """Test that passing theta start, stop, step creates an expected list of theta values"""
+        obj = Hypocycloid(
+            R = 300,
+            r = 200,
+            theta_start=0,
+            theta_stop=1
+        )
+
+    def test_multiple_thetas_exception(self, thetas: "np.array") -> None:
+        """Test that passing multiple definitions of setting theta raises a ValueError"""
+        with pytest.raises(ValueError):
+            obj = Hypocycloid(
+                R = 300,
+                r = 200,
+                thetas=thetas,
+                theta_start=1,
+                theta_stop=10,
+                theta_step=1
+            )
