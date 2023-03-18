fd8b83d89aa8f58c064aad0aefd1bb6bf142c371
Christopher Greening <chris@christophergreening.com>
2023-03-18 11:10:54 -0400

    ADD: added import of objects

---
+++ tests/test_hypotrochoid.py
@@ -0,0 +1,8 @@
+import pytest
+
+from spyrograph.hypotrochoid.hypotrochoid import Hypotrochoid
+from spyrograph.hypotrochoid.hypocycloid import Hypocycloid
+from spyrograph.hypotrochoid.deltoid import Deltoid
+from spyrograph.hypotrochoid.astroid import Astroid
+from spyrograph.hypotrochoid.ellipse import Ellipse
+from spyrograph.hypotrochoid.tusi_couple import TusiCouple
