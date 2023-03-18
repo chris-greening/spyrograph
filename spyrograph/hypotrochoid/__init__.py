4edeefa1f971086520487e7416f42bf89ba506ab
Christopher Greening <chris@christophergreening.com>
2023-03-18 09:59:36 -0400

    ADD: added imports of hypotrochoid objects to hypotrochoid __init___.py

---
+++ spyrograph/hypotrochoid/__init__.py
@@ -0,0 +1,6 @@
+from spyrograph.hypotrochoid.hypotrochoid import Hypotrochoid
+from spyrograph.hypotrochoid.hypocycloid import Hypocycloid
+from spyrograph.hypotrochoid.deltoid import Deltoid
+from spyrograph.hypotrochoid.astroid import Astroid
+from spyrograph.hypotrochoid.ellipse import Ellipse
+from spyrograph.hypotrochoid.tusi_couple import TusiCouple
