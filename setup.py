61927c6eed67bc090709de18db7f8bda99448229
Christopher Greening <chris@christophergreening.com>
2023-03-18 11:38:14 -0400

    MODIFY: incr minor version number to 0.12.0

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding='UTF-8') as fh:
 
 setuptools.setup(
     name="spyrograph",
-    version="0.11.3",
+    version="0.12.0",
     author="Chris Greening",
     author_email="chris@christophergreening.com",
     description="Library for drawing spirographs in Python",
