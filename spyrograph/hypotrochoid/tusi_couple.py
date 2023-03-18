3706023cc0f9167c132074da015eadd253f0c1be
Christopher Greening <chris@christophergreening.com>
2023-03-18 13:44:22 -0400

    FIX: removed test break to make sure GH Action is triggering as expected

--- spyrograph/hypotrochoid/tusi_couple.py
+++ spyrograph/hypotrochoid/tusi_couple.py
@@ -6,7 +6,7 @@ from numbers import Number
 from typing import List
 
 from spyrograph.hypotrochoid.hypotrochoid import Hypotrochoid
-test
+
 class TusiCouple(Hypotrochoid):
     """Model of a Tusi couple which is a special case of a hypotrochoid where
     the rolling circle has 1/2 the radius of the fixed circle and the distance
