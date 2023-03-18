e58cea6236485edfce014d80d34add13dece82ca
Christopher Greening <chris@christophergreening.com>
2023-03-18 10:02:00 -0400

    ADD: added references to epitrochoid objects

--- spyrograph/__init__.py
+++ spyrograph/__init__.py
@@ -1,6 +1,1 @@
 """Import top-level API"""
-from spyrograph.epitrochoid import Epitrochoid
-from spyrograph.epicycloid import Epicycloid
-from spyrograph.cardioid import Cardioid
-from spyrograph.nephroid import Nephroid
-from spyrograph.ranuncloid import Ranuncloid
