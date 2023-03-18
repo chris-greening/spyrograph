f8f5986de2dbda7d61246c0f35e0a8a8b91ddec4
Christopher Greening <chris@christophergreening.com>
2023-03-18 12:10:36 -0400

    ADD: added numpy as a required install

--- setup.py
+++ setup.py
@@ -15,7 +15,7 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/chris-greening/spyrograph",
     packages=setuptools.find_packages(),
-    install_requires=[],
+    install_requires=["numpy"],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
