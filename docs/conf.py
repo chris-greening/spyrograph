00b73ed4007e6c453b5a0770de6a45f3148f3b97
Christopher Greening <chris@christophergreening.com>
2023-03-18 10:08:40 -0400

    MODIFY: made changes to make pylint happy

--- docs/conf.py
+++ docs/conf.py
@@ -1,4 +1,4 @@
-# Configuration file for the Sphinx documentation builder.
+"""Configuration file for the Sphinx documentation builder.
 #
 # This file only contains a selection of the most common options. For a full
 # list see the documentation:
@@ -9,7 +9,9 @@
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
-#
+"""
+# pylint: disable=invalid-name,redefined-builtin
+
 import os
 import sys
 sys.path.insert(0, os.path.abspath('..'))
