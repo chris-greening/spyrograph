481034da2b28ab492a79aff33884ccd77e33d041
Christopher Greening <chris@christophergreening.com>
2023-03-20 22:24:02 -0400

    ADD: added _set_int_to_list for vals

--- spyrograph/_trochoid.py
+++ spyrograph/_trochoid.py
@@ -233,7 +233,9 @@ class _Trochoid(ABC):
         if inputs[True] > 1:
             raise ValueError("More than one input variable was varied. Please only pass one list of varying inputs and try again.")
 
-        
+        R = self._set_int_to_list(R)
+        r = self._set_int_to_list(r)
+        d = self._set_int_to_list(d)
 
     def _set_int_to_list(self, input_val: Union[Number, List[Number]]) -> List[Number]:
         """Return list of numbers from given input parameter"""
