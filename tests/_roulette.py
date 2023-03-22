a83aab2f1570841a20d6babac681c3a23b8965d9
Christopher Greening <chris@christophergreening.com>
2023-03-21 23:30:48 -0400

    ADD: added test_set_int_to_list unit testing

--- tests/_roulette.py
+++ tests/_roulette.py
@@ -21,6 +21,15 @@ class _TestGeneral:
             thetas=thetas
         )
 
+    def test_set_int_to_list(self):
+        """Test that setting int to list"""
+        num_test = self.class_name._set_int_to_list(1)
+        list_test = self.class_name._set_int_to_list([2])
+        assert isinstance(num_test, list)
+        assert isinstance(list_test, list)
+        assert num_test[0] == 1
+        assert list_test[0] == 2
+
     def test_create_range_multiple_arguments_exception(self, thetas):
         """Test that passing multiple parameters raises an error"""
         with pytest.raises(ValueError):
