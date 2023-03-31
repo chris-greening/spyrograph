b2108c1c6011f2774e0593ee3bb42b3967ab6247
Jorge Zamora <113067058+jbxamora@users.noreply.github.com>
2023-03-30 20:20:28 -0700

    Update epicycloid.py

--- spyrograph/epitrochoid/epicycloid.py
+++ spyrograph/epitrochoid/epicycloid.py
@@ -11,12 +11,11 @@ from spyrograph._cycloid import _Cycloid
 class Epicycloid(_Cycloid, Epitrochoid):
     """A class that represents an epicycloid, which is a curve traced by a point on a circle
     that rolls around the outside of a fixed circle. This is a special case of an epitrochoid
-    where the rolling circle has a radius that is one-third the radius of the fixed circle.
+    where the rolling circle has a radius.
     
     The Epicycloid class allows you to generate points along the curve using mathematical
     equations, and visualize the curve in a plot. It also provides properties and methods
-    for computing the length, area, and curvature of the curve, and for plotting and animating
-    the curve.
+    for plotting and animating the curve.
     
     This class is a powerful tool for exploring the properties and behaviors of epicycloids,
     and can be used in a variety of applications such as in mechanical engineering and
