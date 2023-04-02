f7d49e433db0a7e9a3b5383ce2a783f52f708c7f
chris-greening <chrisgreening3@gmail.com>
2023-04-02 13:15:11 -0400

    ADD: added _misc module for moving some functionality into

---
+++ spyrograph/_misc.py
@@ -0,0 +1,19 @@
+
+
+def _get_products_of_inputs(self, *args) -> Tuple[Number]:
+        """Return a list of tuples that contains all of the input arguments"""
+        list_of_lists = [_set_int_to_list(el) for el in args]
+        product = itertools.product(*list_of_lists)
+        return product
+
+def _validate_only_one_iterable(*args) -> bool:
+    """Return validation check that only one argument passed to create_range is an iterable"""
+    inputs = collections.Counter([isinstance(el, collections.abc.Iterable) for el in args])
+    if inputs[True] > 1:
+        raise ValueError("More than one input variable was varied. Please only pass one list of varying inputs and try again.")
+
+def _set_int_to_list(input_val: Union[Number, List[Number]]) -> List[Number]:
+    """Return list of numbers from given input parameter"""
+    if isinstance(input_val, Number):
+        input_val = [input_val]
+    return input_val
