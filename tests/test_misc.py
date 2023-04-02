cec5726bdc36a0060079f0175f0f396ee8cc4440
chris-greening <chrisgreening3@gmail.com>
2023-04-02 13:28:39 -0400

    FIX: fixed test to work for set_int_to_list

--- tests/test_misc.py
+++ tests/test_misc.py
@@ -1,7 +1,9 @@
-def test_set_int_to_list(self):
+from spyrograph._misc import _set_int_to_list
+
+def test_set_int_to_list():
     """Test that setting int to list"""
-    num_test = self.class_name._set_int_to_list(1)
-    list_test = self.class_name._set_int_to_list([2])
+    num_test = _set_int_to_list(1)
+    list_test = _set_int_to_list([2])
     assert isinstance(num_test, list)
     assert isinstance(list_test, list)
     assert num_test[0] == 1
