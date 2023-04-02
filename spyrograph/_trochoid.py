7091caa207d5b5f36711b67a043e9b45ffccf7ea
chris-greening <chrisgreening3@gmail.com>
2023-04-02 12:57:23 -0400

    ADD: added ValueError raise if more than one iterable passed to _validate_only_one_iterable

--- spyrograph/_trochoid.py
+++ spyrograph/_trochoid.py
@@ -517,6 +517,8 @@ class _Trochoid(ABC):
     def _validate_only_one_iterable(*args) -> bool:
         """Return validation check that only one argument passed to create_range is an iterable"""
         inputs = collections.Counter([isinstance(el, collections.abc.Iterable) for el in args])
+        if inputs[True] > 1:
+            raise ValueError("More than one input variable was varied. Please only pass one list of varying inputs and try again.")
 
     @staticmethod
     def _set_int_to_list(input_val: Union[Number, List[Number]]) -> List[Number]:
