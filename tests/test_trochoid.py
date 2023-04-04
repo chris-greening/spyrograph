bf9358deee8f2414c1f2ed4ac1ed35b39ad82467
Christopher Greening <chris@christophergreening.com>
2023-04-03 23:35:57 -0400

    MODIFY: moved trochoids into same file and cycloids into same file

--- tests/test_trochoid.py
+++ tests/test_trochoid.py
@@ -4,11 +4,14 @@ from tests._roulette import _TestGeneral, _TestSpecial
 from spyrograph.epitrochoid.epitrochoid import Epitrochoid
 from spyrograph.epitrochoid.epicycloid import Epicycloid
 
+class TestHypotrochoid(_TestGeneral):
+    class_name = Hypotrochoid
+
+    def test_circle_offset(self, instance):
+        assert instance._circle_offset() == 100
+
 class TestEpitrochoid(_TestGeneral):
     class_name = Epitrochoid
 
     def test_circle_offset(self, instance):
         assert instance._circle_offset() == 500
-
-class TestEpicycloid(_TestSpecial, TestEpitrochoid):
-    class_name = Epicycloid
