56ee0dc04a708818e139afbaeea019f40b29f68c
Christopher Greening <chris@christophergreening.com>
2023-04-04 20:00:04 -0400

    MODIFY: modified ValueError call

--- spyrograph/_misc.py
+++ spyrograph/_misc.py
@@ -13,7 +13,10 @@ def _validate_only_one_iterable(*args) -> bool:
     """Return validation check that only one argument passed to create_range is an iterable"""
     inputs = collections.Counter([isinstance(el, collections.abc.Iterable) for el in args])
     if inputs[True] > 1:
-        raise ValueError("More than one input variable was varied. Please only pass one list of varying inputs and try again.")
+        raise ValueError((
+            "More than one input variable was varied.
+            "Please only pass one list of varying inputs and try again."
+        ))
 
 def _set_int_to_list(input_val: Union[Number, List[Number]]) -> List[Number]:
     """Return list of numbers from given input parameter"""
