43f4c8daa5de9fc0b9ea6c859c375734511fe6e5
Christopher Greening <chris@christophergreening.com>
2023-04-07 00:07:40 -0400

    MODIFY: incr patch version number to 0.25.2

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding='UTF-8') as fh:
 
 setuptools.setup(
     name="spyrograph",
-    version="0.25.1",
+    version="0.25.2",
     author="Chris Greening",
     author_email="chris@christophergreening.com",
     description="Library for drawing spirographs in Python",
