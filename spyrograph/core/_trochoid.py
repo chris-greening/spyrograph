0098fa8150714bd40458d807781f7c50867d1891
chris-greening <chrisgreening3@gmail.com>
2023-07-01 12:14:03 -0400

    ADD: added type hint for list of arrays

--- spyrograph/core/_trochoid.py
+++ spyrograph/core/_trochoid.py
@@ -34,7 +34,7 @@ class _Trochoid(ABC):
             self, R: Number, r: Number, d: Number, thetas: List[Number] = None,
             theta_start: Number = None, theta_stop: Number = None,
             theta_step: Number = None, origin: Tuple[Number, Number] = (0, 0),
-            orientation: Number = 0, noise: "np.array" = None
+            orientation: Number = 0, noise: List["np.array", "np.array"] = None
         ) -> None:
         """Model of a trochoid curve from given input parameters. A trochoid is
         a curve drawn by tracing a point from a circle as it rolls around the
@@ -221,7 +221,7 @@ class _Trochoid(ABC):
     def add_noise(self, x_scale, y_scale) -> Union["_Trochoid", "_Cycloid"]:
         x_noise = np.random.normal(0, x_scale, size=len(self.x))
         y_noise = np.random.normal(0, y_scale, size=len(self.y))
-        noise = list(zip(x_noise, y_noise))
+        noise = [x_noise, y_noise]
         try:
             rotated_shape = self.__class__(
                 R=self.R,
